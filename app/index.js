f7a42bbae6ae6325d7577f62c4204ad5140326f4
mohamedali69 <dalichakhari6801@gmail.com>
2024-02-01 12:57:08 +0100

    Update layout and add welcome section

--- app/index.js
+++ app/index.js
@@ -15,13 +15,24 @@ const Home = () => {
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
         options={{
-          headerStyle: {
-            backgroundColor: COLORS.lightWhite,
-            headerShadowVisible: false,
-            headerLeft: null,
-          },
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
         }}
       />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,60 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
+import { SIZES, icons } from "../../../constants";
+import { FlatList } from "react-native-gesture-handler";
+import { useRouter } from "expo-router";
+
+const jobTypes = ["Full Time", "Part Time", "Internship", "Freelance"];
 
 const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full Time");
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text styles={styles.useName}>Hello Adrian</Text>
+        <Text styles={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+            placeholder="Search job title or keyword"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={jobTypes}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.tab(activeJobType, item)}
+            onPress={() => {
+              setActiveJobType(item);
+              router.push(`/search/${item}`);
+            }}
+          >
+            <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item}
+        contentContainerStyle={{ columnGap: SIZES.small }}
+        horizontal
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
